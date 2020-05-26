476817d7407079127237bb9539693598b43704f0
Krivius <krivtskiy_d@mail.ru>
2020-05-26 18:37:51 +0300

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Buttons from './Buttons';
+
+function Header() {
+    return (
+        <footer>
+            <h2>Here shall be footer!!!</h2>
+           
+        </footer>
+    );
+
+}
+
+export default Header;
